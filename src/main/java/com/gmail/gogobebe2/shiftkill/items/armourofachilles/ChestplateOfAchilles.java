a7ec547350538bb2b11cb3a17c6b5ac6505d53f7
gogobebe2 <gogobebe2@gmail.com>
2015-07-24 07:54:18 +1200

    1.0.1-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftkill/items/armourofachilles/ChestplateOfAchilles.java
+++ src/main/java/com/gmail/gogobebe2/shiftkill/items/armourofachilles/ChestplateOfAchilles.java
@@ -4,6 +4,7 @@ import com.gmail.gogobebe2.shiftkill.items.EnhancedItemStack;
 import org.bukkit.ChatColor;
 import org.bukkit.Material;
 import org.bukkit.enchantments.Enchantment;
+import org.bukkit.entity.Player;
 import org.bukkit.inventory.meta.ItemMeta;
 
 import java.util.ArrayList;
@@ -12,6 +13,13 @@ import java.util.Random;
 
 public class ChestplateOfAchilles extends EnhancedItemStack {
     private int evasionPercentageChance = 10;
+
+    /**
+     * @param holder Can be {@code null} if there is no holder.
+     */
+    protected ChestplateOfAchilles(Player holder) {
+        super(holder);
+    }
 
     /**
      * This method gives a certain chance of evasion for each attacks.
--- src/main/java/com/gmail/gogobebe2/shiftkill/items/armourofachilles/PotionOfAchilles.java
+++ src/main/java/com/gmail/gogobebe2/shiftkill/items/armourofachilles/PotionOfAchilles.java
@@ -2,8 +2,16 @@ package com.gmail.gogobebe2.shiftkill.items.armourofachilles;
 
 import com.gmail.gogobebe2.shiftkill.items.EnhancedItemStack;
 import org.bukkit.Material;
+import org.bukkit.entity.Player;
 
 public class PotionOfAchilles extends EnhancedItemStack {
+
+    /**
+     * @param holder Can be {@code null} if there is no holder.
+     */
+    protected PotionOfAchilles(Player holder) {
+        super(holder);
+    }
 
     @Override
     protected void useAbility() {
