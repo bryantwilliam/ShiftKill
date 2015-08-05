fec666b0f2b67a0fd279b5f05890323046814571
gogobebe2 <gogobebe2@gmail.com>
2015-08-05 20:45:21 +1200

    1.3.0-DEVELOPMENT-SNAPSHOT

--- src/main/java/com/gmail/gogobebe2/shiftkill/kits/ArmourOfAchilles.java
+++ src/main/java/com/gmail/gogobebe2/shiftkill/kits/ArmourOfAchilles.java
@@ -2,12 +2,18 @@ package com.gmail.gogobebe2.shiftkill.kits;
 
 import com.gmail.gogobebe2.shiftkill.kits.items.ChestplateOfAchilles;
 import com.gmail.gogobebe2.shiftkill.kits.items.PotionsOfAchilles;
+import org.bukkit.ChatColor;
 
 public final class ArmourOfAchilles extends Kit {
 
     @Override
-    public void initItems() {
-        getItems().add(new ChestplateOfAchilles());
-        getItems().add(new PotionsOfAchilles());
+    protected void initItems() {
+        getITEMS().add(new ChestplateOfAchilles());
+        getITEMS().add(new PotionsOfAchilles());
+    }
+
+    @Override
+    protected String initName() {
+        return ChatColor.RED + "Armour Of Achilles";
     }
 }
--- src/main/java/com/gmail/gogobebe2/shiftkill/kits/Kit.java
+++ src/main/java/com/gmail/gogobebe2/shiftkill/kits/Kit.java
@@ -1,5 +1,6 @@
 package com.gmail.gogobebe2.shiftkill.kits;
 
+import org.bukkit.ChatColor;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 
@@ -8,7 +9,8 @@ import java.util.Queue;
 
 public abstract class Kit {
     // Sorted by priority.
-    private Queue<ItemStack> items = new LinkedList<>();
+    private final Queue<ItemStack> ITEMS = new LinkedList<>();
+    private final String NAME = initName();
 
     protected Kit() {
         initItems();
@@ -20,21 +22,31 @@ public abstract class Kit {
     protected abstract void initItems();
 
     /**
+     * This method should return the name of the {@code Kit}.
+     */
+    protected abstract String initName();
+
+    /**
      * Gives {@code player} a set based on each individual item's priority.
      * @param player The {@code Player} to recieve the ItemStacks.
      */
     public void giveSet(Player player) {
-        for (ItemStack item : items) {
+        for (ItemStack item : ITEMS) {
             for (ItemStack drop : player.getInventory().addItem(item).values()) {
-                player.getWorld().dropItem(player.getLocation().clone().add(0, 3.5, 0), drop);
+                player.getWorld().dropItem(player.getLocation().getBlock().getLocation(), drop);
             }
         }
+        player.sendMessage(ChatColor.DARK_GREEN + NAME + ChatColor.DARK_GREEN + " received!");
     }
 
     /**
      * @return The queue that holds each ItemStack prioritized.
      */
-    protected Queue<ItemStack> getItems() {
-        return items;
+    protected final Queue<ItemStack> getITEMS() {
+        return this.ITEMS;
+    }
+
+    protected final String getNAME() {
+        return this.NAME;
     }
 }
